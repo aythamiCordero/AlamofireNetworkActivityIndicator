c111b1fac5b0e9eb2d27c4ab3846fbb80865f690
Aythami Cordero Soto <aythami.cordero@bitboxonline.com>
2020-01-22 15:59:29 +0000

    add path

--- Package.swift
+++ Package.swift
@@ -26,6 +26,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
         name: "AlamofireNetworkActivityIndicator",
-        dependencies: [ "Alamofire" ])
+        dependencies: [ "Alamofire" ],
+        path: "Source")
     ]
 )
